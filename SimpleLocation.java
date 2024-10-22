23e967b4d09613b828a0b89c5950cb623e582e6b
VHT QLCL K3 <thuhien.bk0301@gmail.com>
2024-10-22 17:32:10 +0700

    Practice Create Object and Class

--- SimpleLocation.java
+++ SimpleLocation.java
@@ -5,7 +5,16 @@ public class SimpleLocation {
         this.latitute = lat;
         this.longtitute = lon;
     }
-    public double Distance(SimpleLocation other){
-        
+    public static void main(String[] args){
+        double d = -77.0;
+        SimpleLocation lima = new SimpleLocation(-12.0, d);
+        SimpleLocation washDC = new SimpleLocation(38.9, lima.longtitute);
+
+        SimpleLocation loc1 = new SimpleLocation(39.9, 116.4);
+        SimpleLocation loc2 = new SimpleLocation(55.5, 37.6);
+        loc1 = loc2;
+        loc1.latitute = -8.3;
+        System.out.println(loc2.latitute + ", " + loc2.longtitute);
     }
 }
+
